c8da2d378347bdec9caf52c41c107276cca3d2fc
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-09 10:10:15 +0530

    feat: add navbar with search input UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <NavBar />
+      App
     </div>
   );
 }
 
 export default App;
+function NavBar() {
+  return (
+    <nav>
+      <h1>🍿 usePopcorn</h1>
+
+      <input
+        type="text"
+        placeholder="Search movies..."
+      />
+    </nav>
+  );
+}
+
