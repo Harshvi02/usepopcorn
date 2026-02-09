944a0df44174fd885cd9c13bcc0152fa133ce01a
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-09 12:26:07 +0530

    feat: render movie list in left box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+
 import "./index.css";
+const tempMovies = [
+  {
+    imdbID: "tt4154796",
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwODk5MTU3NzM@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMmFlZjQwYzItN2NhMC00ZDJhLTg1YzUtYzZjZjNjOGZjNWJhXkEyXkFqcGc@._V1_SX300.jpg",
+  },
+];
 
 export default function App() {
   return (
@@ -22,8 +37,33 @@ function NavBar() {
 function Main() {
   return (
     <main className="main">
-      <div className="box">Movie List</div>
-      <div className="box">Watched List</div>
+      <div className="box">
+        <MovieList movies={tempMovies} />
+      </div>
+
+      <div className="box">
+        Watched List
+      </div>
     </main>
   );
 }
+
+function MovieList({ movies }) {
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <Movie key={movie.imdbID} movie={movie} />
+      ))}
+    </ul>
+  );
+}
+function Movie({ movie }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt={movie.Title} width="40" />
+      
+      <p>{movie.Year}</p>
+    </li>
+  );
+}
+
